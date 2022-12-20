7041a1213b3c6d33009d59d201ea549e04329e39
omolinsc <oriol.molins@bootcamp-upgrade.com>
2022-12-20 10:46:09 +0100

    ITERATION#2 - Done with all class and Santi

--- index2.js
+++ index2.js
@@ -2,77 +2,112 @@
 
 //* 2.1 Inserta dinamicamente en un html un div vacio con javascript.
 
+// crear el div ✅
+// insertarlo en el HTML ✅
+
 const createDiv$$ = document.createElement("div");
-console.log(createDiv$$);           //! No veo el resultado en el HTML
+document.body.appendChild(createDiv$$);
 
 //* 2.2 Inserta dinamicamente en un html un div que contenga una p con javascript.
 
+// crear el DIV ✅
+// insertarlo en el HTML ✅
+// meter la P en el DIV ✅
+// insertarlo el DIV en el HTML ✅
+
 const createDiv2$$ = document.createElement("div");
 const createP$$ = document.createElement("p");
-const showP$$ = createDiv2$$.appendChild(createP$$);
-console.log(showP$$);               //! No veo el resultado en el HTML
+createDiv2$$.appendChild(createP$$);
+document.body.appendChild(createDiv2$$);
 
 //* 2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un loop con javascript.
+
+// crear el DIV ✅
+// insertarlo en el HTML ✅
+// meter la P en el DIV ✅
+// insertar el DIV en el HTML ✅
 
 const createDiv3$$ = document.createElement("div");
 for (let i = 0; i < 6; i++) {
     let createP3$$ = document.createElement("p");
-    createDiv3$$.appendChild(createP3$$);
-    
+    createDiv3$$.appendChild(createP3$$);    
 }
-console.log(createDiv3$$);          //! No veo el resultado en el HTML
-
+document.body.appendChild(createDiv3$$);
 
 //* 2.4 Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.
 
+// crear el P ✅
+// añadir texto a P ✅
+// insertar el P en el HTML ✅
+
 const createP4$$ = document.createElement("p");
 createP4$$.innerText = "Soy dinámico!";
-console.log(createP4$$);            //! No veo el resultado en el HTML
+document.body.appendChild(createP4$$);
 
 //* 2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
 
-const getH2$$ = document.getElementsByTagName("h2");
+// localizar el H2 con la clase elegida ✅
+// añadir texto a H2 ✅
+
+const getH2$$ = document.querySelector("h2.fn-insert-here");
 getH2$$.textContent = "Wubba Lubba dub dub";
-console.log(getH2$$);               //! No veo el resultado en el HTML
 
 //* 2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
 
+// crear un UL ✅
+// hacer un bucle que cree los LI ✅
+// en el bucle añadir las apps en el LI ✅
+// hacerlo realidad ✅
+
+
 const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+
 const uList$$ = document.createElement("ul");
 for (let i = 0; i < apps.length; i++){
-    const iList$$ = document.createElement("il");
+    const iList$$ = document.createElement("li");
     uList$$.appendChild(iList$$);
     iList$$.textContent = apps[i];
 }
-console.log(uList$$);               //! No veo el resultado en el HTML
+document.body.appendChild(uList$$);
 
 //* 2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
 
-const deleteByClass$$ = document.getElementsByClassName("fn-remove-me");
+// seleccionar TODOS los objetos con la clase fn-remove-me ✅
+// hacer un bucle para borrar uno a uno los elementos ✅
+
+const deleteByClass$$ = document.querySelectorAll(".fn-remove-me");
 for ( let i = 0; i < deleteByClass$$.length; i++){
     deleteByClass$$[i].remove();
 }
-console.log(deleteByClass$$);       //! Quedan 2 por eliminar y no sé el motivo
 
 //* 2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. 
 //* 	Recuerda que no solo puedes insertar elementos con .appendChild.
 
-const selectDivs$$ = document.getElementsByTagName("div");
+// crear un P ✅
+// insertar texto a P ✅
+// seleccionar los DIV ✅
+// insertar P entre los DIV ✅
+// hacerlo realidad ✅
+
 const newP$$ = document.createElement("p");
-newP$$.textContent = ("Voy en medio!");
-selectDivs$$[1].appendChild(newP$$);
-// document.body.insertBefore(newP$$,selectDivs$$);
-console.log(newP$$);                //! No sé cómo funciona ni hacerlo sin appendChild
+newP$$.textContent = "Voy en medio!";
+const selectDivs$$ = document.querySelectorAll("div");
+// la posición 1 de mis selectDivs$$ es el que va detrás justo de la P
+document.body.insertBefore(newP$$,selectDivs$$[1]);
 
 //* 2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase .fn-insert-here
 
-const selectDivsByClass$$ = document.getElementsByClassName("fn-insert-here");
-const pInside$$ = document.createElement("p");
-pInside$$.textContent = ("Voy dentro!");
-for ( let i = 0; i < selectDivsByClass$$.length; i++){
-selectDivsByClass$$[i].appendChild(pInside$$);
+// crear un P ✅
+// insertar texto a P ✅
+// seleccionar los DIV con la clase fn-insert-here ✅
+// insertar P en los DIV ✅
+// hacerlo realidad ✅
+
+const selectedDivs$$ = document.querySelectorAll(".fn-insert-here");
+for ( const div of selectedDivs$$){
+    const pInside$$ = document.createElement("p");
+    pInside$$.textContent = "Voy dentro!";
+    div.appendChild(pInside$$);
 }
 
-
-console.log(selectDivsByClass$$.length);   //! Me lo pone en un div pero no en el otro
 
